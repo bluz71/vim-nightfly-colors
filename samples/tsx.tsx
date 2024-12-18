f1176605eb01b38d84e0e9e221c9599bd022dfd4
Dennis B <bluz71@users.noreply.github.com>
2024-12-18 13:58:58 +1100

    style: highlight React component tags as types

--- samples/tsx.tsx
+++ samples/tsx.tsx
@@ -2,15 +2,17 @@ import * as React from "react";
 import UserInterface from '../UserInterface'
 export default class UserComponent extends React.Component<UserInterface, {}> {
 
-constructor (props: UserInterface){
-  super(props);
-}
+  constructor (props: UserInterface){
+    super(props);
+  }
 
-render() {
-  return (
-    <div>
-      <title>Astro Example</title>
-      <h1>User Component</h1>
+  render() {
+    return (
+      <div>
+        <Example />
+        <title>Astro Example</title>
+        <h1>User Component</h1>
+
         Hello, <b>{this.props.name}</b>
         <br/>
         You are <b>{this.props.age} years old</b>
@@ -18,7 +20,7 @@ render() {
         You live at: <b>{this.props.address}</b>
         <br/>
         You were born: <b>{this.props.dob.toDateString()}</b>
-    </div>
+      </div>
     );
   }
 }
